ef7eb8316db2a1c70380e02942378b6d8175a281
jeetsavsani1173 <jeetsavsani1173@gmail.com>
2023-03-24 00:44:20 +0530

    Uniqe roomId generator functionality added with toast

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 import EditorPage from "./pages/EditorPage";
 
 function App() {
   return (
     <>
+      <div>
+        <Toaster position="top-right"></Toaster>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,44 @@
-import React from "react";
-// import { MDBInput } from "mdb-react-ui-kit";
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+import { toast } from "react-hot-toast";
 
 function Home() {
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    // console.log(id);
+    setRoomId(id);
+    toast.success("Created a new room");
+  };
   return (
     <div className="homePageWrapper">
       <img className="image1" src="/background.jpg" alt="logo" />
       <div className="formWrapper">
         <img className="homePageLogo" src="/logo-3.png" alt="logo" />
-        {/* <img src="/logo-2.png" alt="logo" height={"150px"} /> */}
         <h4 className="mainLabel">Paste Invitation ROOM ID:</h4>
         <div className="inputGroup">
-          <input type="text" className="inputBox" placeholder="ROOM ID" />
-          <input type="text" className="inputBox" placeholder="USERNAME" />
+          <input
+            type="text"
+            className="inputBox"
+            value={roomId}
+            onChange={(e) => setRoomId(e.target.value)}
+            placeholder="ROOM ID"
+          />
+          <input
+            type="text"
+            className="inputBox"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="USERNAME"
+          />
           <div className="btnP">
             <button className="btn joinBtn">Join</button>
           </div>
           <span className="createInfo">
             If you don't have an invite then create &nbsp;
-            <a href="/" className="createNewBtn">
+            <a onClick={createNewRoom} href="/" className="createNewBtn">
               New Room
             </a>
           </span>
