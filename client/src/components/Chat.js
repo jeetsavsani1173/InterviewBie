b9fd6d2a9d8b6ed53c2c41696b669c121a974428
jeetsavsani1173 <jeetsavsani1173@gmail.com>
2023-05-03 17:44:23 +0530

    send message button added + toggle for chat and video added

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 function Chat({ socketRef, roomId }) {
+  const [toggle, setToggle] = useState("chat");
   const [messageList, setMessageList] = useState([]);
   const [typed, setTyped] = useState("");
   const location = useLocation();
@@ -27,7 +28,57 @@ function Chat({ socketRef, roomId }) {
     socketRef?.current?.on("receive_message", (data) => {
       setMessageList((list) => [...list, data]);
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [socketRef.current]);
+
+  const inputClicked = () => {
+    // const inputArea = document.getElementById("input");
+    // inputArea.placeholder = "Enter your input here";
+    // inputArea.value = "";
+    // inputArea.disabled = false;
+    // const chatsection = document.getElementById("chat__section");
+    // chatsection.appendChild("videoClass");
+    // chatsection.removeChild("chatClass");
+    const chatLable = document.getElementById("chatLable");
+    const videoLable = document.getElementById("videoLable");
+    chatLable.classList.remove("notClickedLabel");
+    chatLable.classList.add("clickedLabel");
+    videoLable.classList.remove("clickedLabel");
+    videoLable.classList.add("notClickedLabel");
+    setToggle("chat");
+  };
+
+  const outputClicked = () => {
+    // const inputArea = document.getElementById("input");
+    // inputArea.placeholder =
+    //   "You output will apear here, Click 'Run code' to see it";
+    // inputArea.value = "";
+    // inputArea.disabled = true;
+    // const chatsection = document.getElementById("chat__section");
+    // chatsection.appendChild("chatClass");
+    // chatsection.removeChild("videoClass");
+    const chatLable = document.getElementById("chatLable");
+    const videoLable = document.getElementById("videoLable");
+    chatLable.classList.remove("clickedLabel");
+    chatLable.classList.add("notClickedLabel");
+    videoLable.classList.remove("notClickedLabel");
+    videoLable.classList.add("clickedLabel");
+    setToggle("video");
+  };
+
+  const SendMessage = (e) => {
+    e.preventDefault();
+    if (typed) {
+      const messageData = {
+        room: roomId,
+        author: username,
+        message: typed,
+      };
+      socketRef.current.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setTyped("");
+    }
+  };
 
   const handleChange = (e) => {
     setTyped(e.target.value);
@@ -35,43 +86,55 @@ function Chat({ socketRef, roomId }) {
   return (
     <div className="chat">
       <section className="chat__section">
-        <div className="brand">
-          <h3>Wassup Chat of : {username}</h3>
+        <div className="butan">
+          <label id="chatLable" className="clickedLabel" onClick={inputClicked}>
+            Chat
+          </label>
+          <label
+            id="videoLable"
+            className="notClickedLabel"
+            onClick={outputClicked}
+          >
+            Video
+          </label>
         </div>
-        <div className="message__area">
-          {messageList?.map((message) => (
-            <div
-              className={`message ${
-                message.author === username ? "outgoing" : "incoming"
-              }`}
-            >
-              <h4>{message.author}</h4>
-              <p>{message.message}</p>
+        {toggle === "chat" ? (
+          <div className="chatClass">
+            <div className="brand">
+              <h3>Wassup Chat of : {username}</h3>
             </div>
-          ))}
-          {/* <div className="incoming message">
-            <h4>Jeet S</h4>
-            <p>hello How Are you yash ?</p>
+            <div className="message__area">
+              {messageList?.map((message) => (
+                <div
+                  className={`message ${
+                    message.author === username ? "outgoing" : "incoming"
+                  }`}
+                >
+                  <h4>{message.author}</h4>
+                  <p>{message.message}</p>
+                </div>
+              ))}
+            </div>
+            <div className="chatSend">
+              <textarea
+                id="textarea"
+                cols="30"
+                rows="1"
+                value={typed}
+                placeholder="Write a message..."
+                onChange={handleChange}
+                onKeyDown={handleKeyDown}
+              ></textarea>
+              <button className="btn sendBtn" onClick={SendMessage}>
+                Send
+              </button>
+            </div>
           </div>
-          <div className="outgoing message">
-            <h4>Yash P</h4>
-            <p>hye Jeet, I am fine!</p>
-          </div> */}
-        </div>
-        <div>
-          <textarea
-            id="textarea"
-            cols="30"
-            rows="1"
-            value={typed}
-            placeholder="Write a message..."
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
-          ></textarea>
-          {/* <button lable="textarea" onClick={sendMessageHandler} className="btn">
-            send
-          </button> */}
-        </div>
+        ) : (
+          <div className="videoClass">
+            <h1 style={{ color: "white" }}>Video section</h1>
+          </div>
+        )}
       </section>
     </div>
   );
